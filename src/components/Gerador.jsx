b19b7b6ed625afb20e4a7eb8416c14ceddb60344
joaovic <jvfs_1998@hotmail.com>
2025-09-30 20:07:23 -0300

    Faltou atualizar o css

--- src/components/Gerador.jsx
+++ src/components/Gerador.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
-import "./gerador.css";
+import "components/Gerador.css";
 
 const Gerador = () => {
     const [dados, setDados] = useState("");
